1b8817323a9e19612df0e4493f96ff7748a108e6
Thongqi <141812298+Thongqi@users.noreply.github.com>
2026-04-14 17:15:36 +0800

    add in shuffle array function, display score

--- memory-card-game/src/App.jsx
+++ memory-card-game/src/App.jsx
@@ -1,12 +1,22 @@
 // import { useState } from "react";
 
+import { useState } from "react";
 import "./App.css";
 import { DisplayCard } from "./components/display-card";
 
 function App() {
+  const [selected, setSelected] = useState([])
+  const [highestScore, setHighestScore] = useState("0")
+
+  const obj = {selected, setSelected, highestScore, setHighestScore}
+
   return (
     <>
-      <DisplayCard></DisplayCard>{" "}
+      <div>
+            <p>Highest Record: {highestScore} </p>
+            <p>Your score: {selected.length}</p>
+      </div>
+      <DisplayCard {...obj}></DisplayCard>
     </>
   );
 }
--- memory-card-game/src/components/display-card.jsx
+++ memory-card-game/src/components/display-card.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import "./display-card.css";
+import { DisplayScore } from "./count-score";
 
-export function DisplayCard() {
+export function DisplayCard({selected, setSelected, highestScore, setHighestScore}) {
   const [images, setImages] = useState(null);
+  const [cards, setCards] = useState(null)
 
   useEffect(() => {
     const xhr = new XMLHttpRequest();
@@ -15,17 +17,60 @@ export function DisplayCard() {
     xhr.send();
   }, []);
 
-  // create 9 object with link, id, and coordinates
+  if (images){
+    setCards(images.message.map((link, index) => (
+      {id: index + 1, coordinate: null, src: link}
+    )))
+  }
 
+  function handleClick(id){
+    setCards(shuffle(cards))
+    
+    // if the item is selected before, then reset score, and save the current score as highest score
+    if (selected.includes(id)){
+        selected.length > highestScore? setHighestScore(selected.length): null
+        setSelected(id)
+    } else {
+      setSelected([...selected, id])
+    }
+  }
+  
   return (
     <div className="card-container">
       {images
-        ? images.message.map((imagelink, index) => (
-            <div key={index} className="card">
-              <img src={imagelink}></img>
-            </div>
-          ))
+        ? cards.map((index, card) => (
+        <Card index={index} imagelink={card.link} handleClick={handleClick(card.id)}></Card>
+      ))
         : "Loading"}
     </div>
   );
+}
+
+function Card({index, imagelink, setCards, cards, handleClick}){
+  const position = this.getBoundingClientRect()
+
+  setCards(cards => [...cards, {id: index, key: index, coordinate: position, src: imagelink}])
+  
+  return (
+    <div key={index} className="card" onClick={handleClick}>
+      <img src={imagelink}></img>
+    </div>
+  )
+}
+
+function shuffle(array){
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array
 }
