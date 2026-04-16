60f2bad25d7f71980176531634fa5dc734bba246
Thongqi <141812298+Thongqi@users.noreply.github.com>
2026-04-16 21:03:57 +0800

    use framer motion shuffle animation, to show numbering on selected

--- memory-card-game/src/components/display-card.jsx
+++ memory-card-game/src/components/display-card.jsx
@@ -1,5 +1,12 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import "./display-card.css";
+import { motion } from "motion/react";
+
+const spring = {
+  type: "spring",
+  damping: 20,
+  stiffness: 300,
+};
 
 export function DisplayCard({
   selected,
@@ -8,10 +15,6 @@ export function DisplayCard({
   setHighestScore,
 }) {
   const [cards, setCards] = useState(null);
-  const containerRef = useRef();
-  const cardposition = null
-  const containerCenter = null
-  const [movetoCenter, setMovetoCenter] = useState(false)
 
   useEffect(() => {
     fetch("https://dog.ceo/api/breeds/image/random/9")
@@ -27,24 +30,7 @@ export function DisplayCard({
       .catch((error) => console.error("Error fetching images:", error));
   }, []);
 
-  // get position of card after finish rendering
-  useEffect(()=> {
-    if (!containerRef.current || !cards) return;
-
-    cardposition = getPosition(containerRef)
-
-    if (cardposition) setCards((prevCards) => 
-      prevCards.map((card, index) => (
-        {...card, lastposition: cardposition[index]}
-      )))
-
-    containerCenter = getCenter(containerRef)
-  }, [])
-
   function handleClick(id) {
-    // move card to center
-    setMovetoCenter(true);
-
     setCards(shuffle(cards));
 
     // if the item is selected before, then reset score, and save the current score as highest score
@@ -54,27 +40,17 @@ export function DisplayCard({
     } else {
       setSelected([...selected, id]);
     }
-
-    // settimeout and move to new position
   }
 
-  // calculate how much x and y need to be move in for each card in roder to reach center
-  const calculatemove =  cardposition ? cardposition.map((card) => ({
-    x: card.x + containerCenter.centerx,
-    y: card.y + containerCenter.centery
-  })) : null
-
   return (
-    <div className="card-container" ref={containerRef}>
+    <div className="card-container">
       {cards
-        ? cards.map((card, index) => (
+        ? cards.map((card) => (
             <Card
               key={card.id}
               imagelink={card.src}
               handleClick={() => handleClick(card.id)}
-              movetoCenter={movetoCenter}
-              x = {calculatemove[index].x}
-              y = {calculatemove[index].y}
+              selected={selected}
             ></Card>
           ))
         : "Loading"}
@@ -82,9 +58,18 @@ export function DisplayCard({
   );
 }
 
-function Card({ index, imagelink, handleClick, movetoCenter, x, y }) {
+function Card({ index, imagelink, handleClick, selected }) {
   return (
-    <motion.div key={index} className="card" onClick={handleClick} animate={{x: movetoCenter? x : 0, y: movetoCenter? y : 0} }>
+    <motion.div
+      key={index}
+      layout
+      transition={spring}
+      className="card"
+      onClick={handleClick}
+    >
+      <div>
+        <p>{selected.length + 1}</p>
+      </div>
       <img src={imagelink}></img>
     </motion.div>
   );
@@ -108,30 +93,29 @@ function shuffle(array) {
   return array;
 }
 
-function getPosition({containerRef}){
-
-
+function getPosition(containerRef) {
   // convert from htmlcollection to real array in order to map
-  const childArray = [...containerRef.current.children]
+  const childArray = [...containerRef.current.children];
 
   return childArray.map((child) => {
-        const rect = child.getBoundingClientRect()
-        return { 
-          x: rect.x,
-          y: rect.y,
-          width: rect.width,
-          height: rect.height
-        }
-  })
+    const rect = child.getBoundingClientRect();
+    return {
+      x: rect.x,
+      y: rect.y,
+      width: rect.width,
+      height: rect.height,
+    };
+  });
 }
 
-function getCenter({containerRef}){
-    const rect = containerRef.current.getBoundingClientRect()
+function getCenter(containerRef) {
+  const rect = containerRef.current.getBoundingClientRect();
 
-    const childrect = containerRef.current.firstElementChild.getBoundingClientRect()
+  const childrect =
+    containerRef.current.firstElementChild.getBoundingClientRect();
 
-    const centerx = rect.left + rect.width / 2 - (childrect.width / 2);
-    const centery = rect.top + rect.height / 2 - (childrect.height / 2);
+  const centerx = rect.left + rect.width / 2 - childrect.width / 2;
+  const centery = rect.top + rect.height / 2 - childrect.height / 2;
 
-    return [centerx, centery]
+  return [centerx, centery];
 }
